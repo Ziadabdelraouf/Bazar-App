d1b15f81d08d38ffba56eff0fb82a9a44207d0e4
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-07-05 22:43:10 +0300

    feat: add app reusable design colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary50 = Color(0xFFFAF9FD);
+  static const Color primary100 = Color(0xFFE5DEF8);
+  static const Color primary200 = Color(0xFFCABCEF);
+  static const Color primary300 = Color(0xFFA28CE0);
+  static const Color primary400 = Color(0xFF7D64C3);
+  static const Color primary500 = Color(0xFF54408C);
+  static const Color primary600 = Color(0xFF352368);
+  static const Color primary700 = Color(0xFF251554);
+  static const Color primary800 = Color(0xFF10052F);
+  static const Color primary900 = Color(0xFF09031B);
+  
+
+  static const Color grey50 = Color(0xFFFAFAFA);
+  static const Color grey100 = Color(0xFFF5F5F5);
+  static const Color grey200 = Color(0xFFE8E8E8);
+  static const Color grey300 = Color(0xFFD6D6D6);
+  static const Color grey400 = Color(0xFFB8B8B8);
+  static const Color grey500 = Color(0xFFA6A6A6);
+  static const Color grey600 = Color(0xFF7A7A7A);
+  static const Color grey700 = Color(0xFF454545);
+  static const Color grey800 = Color(0xFF292929);
+  static const Color grey900 = Color(0xFF121212);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color yellow = Color(0xFFFBAE05);
+  static const Color orange = Color(0xFFFF8C39);
+  static const Color red = Color(0xFFEF5A56);
+  static const Color blue = Color(0xFF3784FB);
+}
