8170cf63d34fa7765116c35bb4d137beb604d8bb
Ziad Abdelraouf <ziadabdelraouff@gmail.com>
2026-07-13 17:55:19 +0300

    feat: Add Arabic Language

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bazar_group_1/features/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
 import 'core/router/app_router.dart';
 
 void main() {
@@ -12,6 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale("ar"),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       title: 'Bazar App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
