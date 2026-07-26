cc66747b7d4e4ef15870af8db4dbaeac8f8c23b0
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-07-26 04:19:17 +0300

    chore: restore HomePage as default, keep test lines commented

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:bazar_group_1/core/mock/mock_data_reader.dart';
-import 'package:bazar_group_1/core/network/books_dio.dart';
 import 'package:bazar_group_1/features/home/presentation/pages/home_page.dart';
+//import 'package:bazar_group_1/features/home/presentation/widgets/best_vendors_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/localization/generated/l10n.dart';
 import 'core/router/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+//import 'package:bazar_group_1/features/home/presentation/pages/vendors_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,6 +46,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const HomePage(),
+      //home: const VendorsPage(),
+      //home: Scaffold(body: SafeArea(child: Padding(padding: const EdgeInsets.all(16), child: BestVendorsWidget()))),
       routes: AppRouter.routes,
     );
   }
