552ad76d6ba3800d917744f4ed1b85db203b0c8a
Ziadabdelraouf <ziadabdelraouff@gmail.com>
2026-07-29 11:51:41 +0300

    feature : add responsive builder in material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:bazar_group_1/features/forgot_password/presentation/pages/create_new_password_page.dart';
-import 'package:bazar_group_1/features/forgot_password/presentation/pages/forgot_password_page.dart';
 import 'package:bazar_group_1/core/mock/mock_data_reader.dart';
+import 'package:bazar_group_1/core/responsive/app_responsive_breakpoints.dart';
 import 'package:bazar_group_1/core/router/app_router.dart';
 import 'package:bazar_group_1/features/home/presentation/pages/home_page.dart';
 import 'package:bazar_group_1/features/splash_screen/splash_screen.dart';
@@ -8,14 +7,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'core/localization/generated/l10n.dart';
-//import 'package:bazar_group_1/features/home/presentation/pages/vendors_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await loadMockData();
-  WidgetsFlutterBinding.ensureInitialized();
   await loadMockData();
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -27,12 +24,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       builder: (context, child) {
-        return GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: () {
-            FocusManager.instance.primaryFocus?.unfocus();
-          },
-          child: child ?? const SizedBox.shrink(),
+        return ResponsiveBreakpoints.builder(
+          child: GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            child: MaxWidthBox(
+              maxWidth: AppResponsiveBreakpoints.maxContentWidth,
+              child: child ?? const SizedBox.shrink(),
+            ),
+          ),
+          breakpoints: AppResponsiveBreakpoints.breakpoints,
         );
       },
       locale: Locale("en"),
