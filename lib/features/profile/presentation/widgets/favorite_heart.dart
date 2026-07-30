f9115ee03749db4ea59a7e5e5b1ceb8d688d2259
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-07-30 04:53:39 +0300

    feat: add FavoriteHeart widget with pop animation

---
+++ lib/features/profile/presentation/widgets/favorite_heart.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:bazar_group_1/core/theme/app_colors.dart';
+import 'package:bazar_group_1/core/theme/app_icons.dart';
+
+class FavoriteHeart extends StatefulWidget {
+  const FavoriteHeart({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  State<FavoriteHeart> createState() => _FavoriteHeartState();
+}
+
+class _FavoriteHeartState extends State<FavoriteHeart>
+    with SingleTickerProviderStateMixin {
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 300),
+  );
+
+  late final _scale = Tween<double>(begin: 1.0, end: 1.3).animate(
+    CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticOut,
+    ),
+  );
+
+  void _handleTap() {
+    _controller.forward(from: 0);
+    widget.onTap();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: SizedBox(
+        width: 24,
+        height: 24,
+        child: Center(
+          child: ScaleTransition(
+            scale: _scale,
+            child: SvgPicture.asset(
+              AppIcons.loveFill,
+              width: 20,
+              height: 19,
+              colorFilter: const ColorFilter.mode(
+                AppColors.primary500,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
