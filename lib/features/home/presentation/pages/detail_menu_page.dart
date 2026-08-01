09c50406c06e77c220fc3f857a7246db057c441f
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-08-02 00:32:45 +0300

    feat: use FavoriteHeart on Details page with toggle logic

--- lib/features/home/presentation/pages/detail_menu_page.dart
+++ lib/features/home/presentation/pages/detail_menu_page.dart
@@ -5,6 +5,7 @@ import 'package:bazar_group_1/core/theme/app_icons.dart';
 import 'package:bazar_group_1/core/theme/app_text_styles.dart';
 import 'package:bazar_group_1/features/profile/domain/entities/favorite_item.dart';
 import 'package:bazar_group_1/features/profile/presentation/notifiers/favorites_notifier.dart';
+import 'package:bazar_group_1/features/profile/presentation/widgets/favorite_heart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -86,25 +87,32 @@ class _DetailMenuPageState extends State<DetailMenuPage> {
                 ),
                 Consumer(
                   builder: (context, ref, child) {
-                    return IconButton(
-                      onPressed: () {
-                        final priceValue = double.tryParse(
-                              widget.price.replaceAll('\$', ''),
-                            ) ??
-                            0.0;
+                    final isFavorited = ref.watch(favoritesNotifierProvider.select(
+                      (favorites) => favorites.any((item) => item.title == widget.title),
+                    ));
 
-                        ref.read(favoritesNotifierProvider.notifier).addFavorite(
-                              FavoriteItem(
-                                title: widget.title,
-                                imageUrl: widget.imagePath,
-                                price: priceValue,
-                              ),
-                            );
+                    return FavoriteHeart(
+                      isFavorited: isFavorited,
+                      onTap: () {
+                        final notifier = ref.read(favoritesNotifierProvider.notifier);
+
+                        if (isFavorited) {
+                          notifier.removeFavorite(widget.title);
+                        } else {
+                          final priceValue = double.tryParse(
+                                widget.price.replaceAll('\$', ''),
+                              ) ??
+                              0.0;
+
+                          notifier.addFavorite(
+                            FavoriteItem(
+                              title: widget.title,
+                              imageUrl: widget.imagePath,
+                              price: priceValue,
+                            ),
+                          );
+                        }
                       },
-                      icon: SvgPicture.asset(
-                        AppIcons.loveFill,
-                        colorFilter: ColorFilter.mode(AppColors.primary500, BlendMode.srcIn),
-                      ),
                     );
                   },
                 ),
