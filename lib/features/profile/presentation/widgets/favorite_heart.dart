bd5a3adf56b6b5947492184210eb0dbc39e23b93
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-08-04 13:30:57 +0300

    fix: heart animation now returns to normal size using TweenSequence

--- lib/features/profile/presentation/widgets/favorite_heart.dart
+++ lib/features/profile/presentation/widgets/favorite_heart.dart
@@ -22,24 +22,28 @@ class _FavoriteHeartState extends State<FavoriteHeart>
   late final AnimationController _controller;
   late final Animation<double> _scale;
 
+  void _handleTap() {
+    _controller.forward(from: 0);
+    widget.onTap();
+  }
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 450),
     );
-    _scale = Tween<double>(begin: 1.0, end: 1.3).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.elasticOut,
+    _scale = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(begin: 1.0, end: 1.4).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 40,
       ),
-    );
-  }
-
-  void _handleTap() {
-    _controller.forward(from: 0);
-    widget.onTap();
+      TweenSequenceItem(
+        tween: Tween(begin: 1.3, end: 1.0).chain(CurveTween(curve: Curves.elasticOut)),
+        weight: 60,
+      ),
+    ]).animate(_controller);
   }
 
   @override
@@ -50,23 +54,18 @@ class _FavoriteHeartState extends State<FavoriteHeart>
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: SizedBox(
-        width: 24,
-        height: 24,
-        child: Center(
-          child: ScaleTransition(
-            scale: _scale,
-            child: SvgPicture.asset(
-              AppIcons.loveFill,
-              width: 20,
-              height: 19,
-              colorFilter: ColorFilter.mode(
-                widget.isFavorited ? AppColors.primary500 : AppColors.grey400,
-                BlendMode.srcIn,
-              ),
-            ),
+    return IconButton(
+      onPressed: _handleTap,
+      constraints: const BoxConstraints.tightFor(width: 48, height: 48),
+      icon: ScaleTransition(
+        scale: _scale,
+        child: SvgPicture.asset(
+          AppIcons.loveFill,
+          width: 24,
+          height: 24,
+          colorFilter: ColorFilter.mode(
+            widget.isFavorited ? AppColors.primary500 : AppColors.grey400,
+            BlendMode.srcIn,
           ),
         ),
       ),
