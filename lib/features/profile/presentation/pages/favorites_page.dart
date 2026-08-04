c68abb868146dfca857c2cbd99a7c3e079b036dd
Jolie Ehab <jolie.ehabgsg@gmail.com>
2026-08-04 12:20:36 +0300

    fix: inline list/grid layout instead of using widget-returning methods

--- lib/features/profile/presentation/pages/favorites_page.dart
+++ lib/features/profile/presentation/pages/favorites_page.dart
@@ -4,7 +4,6 @@ import 'package:bazar_group_1/core/theme/app_colors.dart';
 import 'package:bazar_group_1/core/theme/app_text_styles.dart';
 import 'package:bazar_group_1/core/localization/generated/l10n.dart';
 import 'package:bazar_group_1/core/components/navigation/app_back_button.dart';
-import 'package:bazar_group_1/features/profile/domain/entities/favorite_item.dart';
 import 'package:bazar_group_1/features/profile/presentation/notifiers/favorites_notifier.dart';
 import 'package:bazar_group_1/features/profile/presentation/widgets/favorite_item_card.dart';
 
@@ -44,55 +43,47 @@ class FavoritesPage extends ConsumerWidget {
                   : LayoutBuilder(
                       builder: (context, constraints) {
                         if (constraints.maxWidth < 500) {
-                          return _buildFavoritesList(favorites);
+                          return ListView.separated(
+                            itemCount: favorites.length,
+                            separatorBuilder: (context, index) => Divider(
+                              color: AppColors.grey200,
+                              thickness: 1,
+                              height: 1,
+                              indent: 16,
+                              endIndent: 16,
+                            ),
+                            itemBuilder: (context, index) {
+                              final item = favorites[index];
+                              return FavoriteItemCard(
+                                key: ValueKey(item.title),
+                                item: item,
+                              );
+                            },
+                          );
                         }
-                        return _buildFavoritesGrid(favorites);
+                        return GridView.builder(
+                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 3,
+                            crossAxisSpacing: 12,
+                            mainAxisSpacing: 8,
+                          ),
+                          itemCount: favorites.length,
+                          itemBuilder: (context, index) {
+                            final item = favorites[index];
+                            return FavoriteItemCard(
+                              key: ValueKey(item.title),
+                              item: item,
+                            );
+                          },
+                        );
                       },
                     ),
             ),
           ],
         ),
       ),
-    );
-  }
-
-    Widget _buildFavoritesList(List<FavoriteItem> favorites) {
-    return ListView.separated(
-      itemCount: favorites.length,
-      separatorBuilder: (context, index) => Divider(
-        color: AppColors.grey200,
-        thickness: 1,
-        height: 1,
-        indent: 16,
-        endIndent: 16,
-      ),
-      itemBuilder: (context, index) {
-        final item = favorites[index];
-        return FavoriteItemCard(
-          key: ValueKey(item.title),
-          item: item,
-        );
-      },
-    );
-  }
-
-  Widget _buildFavoritesGrid(List<FavoriteItem> favorites) {
-    return GridView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3,
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 8,
-      ),
-      itemCount: favorites.length,
-      itemBuilder: (context, index) {
-        final item = favorites[index];
-        return FavoriteItemCard(
-          key: ValueKey(item.title),
-          item: item,
-        );
-      },
     );
   }
 }
