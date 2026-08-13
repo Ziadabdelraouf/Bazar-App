801fc07c101126682a0068bb116b119d81c0858f
Al-Mohanad Ahmed <ahmedmohanad258@gmail.com>
2026-08-13 12:06:38 +0300

    merge: resolve conflicts with dev

--- lib/features/home/presentation/widgets/top_of_week_section.dart
+++ lib/features/home/presentation/widgets/top_of_week_section.dart
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import 'package:bazar_group_1/core/localization/generated/l10n.dart';
-import 'package:bazar_group_1/core/mock/mock_data_reader.dart';
 import 'package:bazar_group_1/core/responsive/app_responsive_breakpoints.dart';
 import 'package:bazar_group_1/core/theme/app_text_styles.dart';
-import 'package:bazar_group_1/features/home/data/models/book_model.dart';
 import 'package:bazar_group_1/features/home/presentation/pages/top_of_week_all_page.dart';
-=======
-import 'package:bazar_group_1/core/responsive/app_responsive_breakpoints.dart';
-import 'package:bazar_group_1/core/theme/app_text_styles.dart';
 import 'package:bazar_group_1/features/home/presentation/providers/books_provider.dart';
->>>>>>> origin/dev
 import 'package:bazar_group_1/features/home/presentation/widgets/book_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,8 +15,7 @@ class TopOfWeekSection extends ConsumerWidget {
     final booksAsync = ref.watch(booksProvider);
 
     final sectionHeight = context.responsiveValue<double>(
-      mobile: (MediaQuery.of(context).size.height * .25)
-          .clamp(180.0, 220.0),
+      mobile: (MediaQuery.of(context).size.height * .25).clamp(180.0, 220.0),
       tablet: 220.0,
       desktop: 240.0,
     );
@@ -58,20 +50,14 @@ class TopOfWeekSection extends ConsumerWidget {
             ),
           ],
         ),
-<<<<<<< HEAD
-        const SizedBox(height: 12),
-=======
 
         const SizedBox(height: 12),
 
->>>>>>> origin/dev
         SizedBox(
           height: sectionHeight,
 
           child: booksAsync.when(
-            loading: () => const Center(
-              child: CircularProgressIndicator(),
-            ),
+            loading: () => const Center(child: CircularProgressIndicator()),
 
             error: (error, stackTrace) => Center(
               child: Text(
@@ -82,16 +68,13 @@ class TopOfWeekSection extends ConsumerWidget {
 
             data: (books) {
               if (books.isEmpty) {
-                return const Center(
-                  child: Text('No books found'),
-                );
+                return const Center(child: Text('No books found'));
               }
 
               return ListView.separated(
                 scrollDirection: Axis.horizontal,
                 itemCount: books.length,
-                separatorBuilder: (context, index) =>
-                    const SizedBox(width: 16),
+                separatorBuilder: (context, index) => const SizedBox(width: 16),
 
                 itemBuilder: (context, index) {
                   final book = books[index];
